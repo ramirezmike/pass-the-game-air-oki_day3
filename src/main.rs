104469bf6f64f3ce45a13b2980af59caccba460d
Thierry Berger <contact@thierryberger.com>
2023-07-26 17:43:13 +0200

    fix ball sometimes not respawning by adding a frame delay to the impulse :shrug: ; made paddle follow mouse ; integrate bevy ci

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,22 @@ use std::collections::VecDeque;
 use std::f32::consts::PI;
 use std::time::Duration;
 
+use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 use bevy::time::Stopwatch;
-use bevy::window::WindowResolution;
+use bevy::window::{PrimaryWindow, WindowResolution};
 
 use bevy_xpbd_2d::prelude::*;
 
-const WINDOW_SIZE: Vec2 = Vec2 {
-    x: 1280.,
-    y: 720.,
-};
+const WINDOW_SIZE: Vec2 = Vec2 { x: 1280., y: 720. };
 
-const PADDLE_SIZE: Vec2 = Vec2 {
-    x: 15.,
-    y: 60.,
-};
+const PADDLE_SIZE: Vec2 = Vec2 { x: 15., y: 60. };
 
 const BALL_RADIUS: f32 = 15.;
 
-const INITIAL_FORCE: f32 = 40000000.;
-const PADDLE_SPEED: f32 = 500.;
+const INITIAL_FORCE: f32 = 20000000.;
+const PADDLE_SPEED: f32 = 5000.;
+const PADDLE_SPEED_AI: f32 = 500.;
 
 #[derive(Resource, Default)]
 struct Score {
@@ -46,6 +42,9 @@ struct Ball {
     kind: BallKind,
 }
 
+#[derive(Component)]
+struct DelayedExternalForce(pub ExternalForce);
+
 #[derive(Clone, Debug)]
 enum BallKind {
     Point,
@@ -57,8 +56,8 @@ enum BallKind {
 impl BallKind {
     fn is_bonus(&self) -> bool {
         match self {
-            Self::Point|Self::Gold => false,
-            _ => true
+            Self::Point | Self::Gold => false,
+            _ => true,
         }
     }
 
@@ -94,7 +93,7 @@ impl Side {
         match self {
             Self::Left => Self::Right,
             Self::Right => Self::Left,
-            _ => Self::Random
+            _ => Self::Random,
         }
     }
 }
@@ -117,30 +116,24 @@ fn main() {
                 }),
                 ..default()
             }),
-            PhysicsPlugins::default()
+            PhysicsPlugins::default(),
         ))
         .insert_resource(Gravity::ZERO)
         .add_systems(Startup, setup)
-        .add_systems(Update, (
+        .add_systems(
+            Update,
             (
-                check_goals,
-                (
-                    respawn_point_ball,
-                    spawn_bonus_ball,
-                ),
-                spawn_ball,
-            ).chain(),
-            move_paddle,
-            print_score
-        ))
+                apply_delayed_external_forces,
+                (check_goals, respawn_point_ball, spawn_ball).chain(),
+                move_paddle,
+                print_score,
+            ),
+        )
         //.add_systems(Update, debug)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    assets: Res<AssetServer>
-) {
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     // Assets
     commands.insert_resource(BallAssets {
         point_ball: assets.load("ball_blue_large.png"),
@@ -154,11 +147,7 @@ fn setup(
     commands.init_resource::<Score>();
 
     // Camera
-    commands.spawn(
-        Camera2dBundle {
-            ..default()
-        }
-    );
+    commands.spawn(Camera2dBundle { ..default() });
 
     // Net (middle line)
     let net_box = Vec2::new(5., WINDOW_SIZE.y);
@@ -173,7 +162,7 @@ fn setup(
         },
         RigidBody::Static,
         Collider::cuboid(net_box.x, net_box.y),
-        CollisionLayers::new([Layer::Net], [Layer::Paddle])
+        CollisionLayers::new([Layer::Net], [Layer::Paddle]),
     ));
 
     // Walls
@@ -190,7 +179,7 @@ fn setup(
     spawn_wall(
         &mut commands,
         0.,
-        - WINDOW_SIZE.y / 2.,
+        -WINDOW_SIZE.y / 2.,
         Vec2::Y,
         CollisionLayers::new([Layer::Wall], [Layer::Paddle, Layer::Ball]),
         false,
@@ -198,7 +187,7 @@ fn setup(
     // Left wall
     spawn_wall(
         &mut commands,
-        - WINDOW_SIZE.x / 2.,
+        -WINDOW_SIZE.x / 2.,
         0.,
         Vec2::X,
         CollisionLayers::new([Layer::Wall], [Layer::Paddle]),
@@ -216,7 +205,7 @@ fn setup(
     // Left goal
     spawn_wall(
         &mut commands,
-        - (WINDOW_SIZE.x / 2. + 5.),
+        -(WINDOW_SIZE.x / 2. + 5.),
         0.,
         Vec2::X,
         CollisionLayers::new([Layer::Wall], [Layer::Ball]),
@@ -237,16 +226,11 @@ fn setup(
     spawn_paddle(&mut commands, false);
 }
 
-fn mirror_transform(
-    transform: &mut Transform
-) {
-    transform.translation.x = - transform.translation.x;
+fn mirror_transform(transform: &mut Transform) {
+    transform.translation.x = -transform.translation.x;
 }
 
-fn spawn_paddle(
-    commands: &mut Commands,
-    first_player: bool,
-) {
+fn spawn_paddle(commands: &mut Commands, first_player: bool) {
     let mut transform = Transform::from_xyz(-(WINDOW_SIZE.x / 2.) + 20., 0., 5.);
     let (color, side) = if first_player {
         // Not colorblind friendly, use images that look different in black and white
@@ -268,15 +252,12 @@ fn spawn_paddle(
         RigidBody::Kinematic,
         Collider::cuboid(PADDLE_SIZE.x, PADDLE_SIZE.y),
         CollisionLayers::new([Layer::Paddle], [Layer::Ball, Layer::Wall, Layer::Net]),
-        Restitution::new(1.),
-        Paddle {
-            first_player,
-            side,
-        },
+        Restitution::new(0.8),
+        Paddle { first_player, side },
     ));
 }
 
-fn spawn_wall (
+fn spawn_wall(
     commands: &mut Commands,
     x: f32,
     y: f32,
@@ -290,7 +271,7 @@ fn spawn_wall (
         RigidBody::Static,
         Collider::halfspace(outward_normal),
         collision_layers,
-        Restitution::new(1.),
+        Restitution::new(0.8),
         Friction::ZERO,
     ));
 
@@ -298,12 +279,12 @@ fn spawn_wall (
         let left = x < 0.;
         wall.insert(Goal {
             first_player: left,
-            side: if left { Side::Left } else { Side::Right }
+            side: if left { Side::Left } else { Side::Right },
         });
     }
 }
 
-fn spawn_ball (
+fn spawn_ball(
     mut commands: Commands,
     ball_assets: Res<BallAssets>,
     mut ball_spawner: ResMut<BallSpawner>,
@@ -314,7 +295,6 @@ fn spawn_ball (
     timer.tick(time.delta());
     if timer.finished() {
         if let Some((ball_kind, _)) = ball_spawner.0.front() {
-
             let ball_collider = Collider::ball(ball_kind.get_radius());
             let ball_position = Vec2::ZERO;
             let intersections = spatial_query.shape_intersections(
@@ -326,14 +306,15 @@ fn spawn_ball (
 
             if intersections.is_empty() {
                 let (ball_kind, spawn_direction) = ball_spawner.0.pop_front().unwrap();
-                
+
                 timer.set_duration(Duration::from_secs(1));
                 timer.reset();
 
-                let direction_angle = rand::random::<f32>() * PI/2. - PI/4.;
+                let direction_angle = rand::random::<f32>() * (PI / 2. - PI / 4.);
                 let mut direction = Vec2::from_angle(direction_angle);
                 if spawn_direction == Side::Left
-                    || (spawn_direction == Side::Random && rand::random::<bool>()) {
+                    || (spawn_direction == Side::Random && rand::random::<bool>())
+                {
                     direction.x = -direction.x;
                 }
 
@@ -351,22 +332,32 @@ fn spawn_ball (
                     RigidBody::Dynamic,
                     ball_collider,
                     CollisionLayers::new([Layer::Ball], [Layer::Ball, Layer::Paddle, Layer::Wall]),
-                    ExternalForce::new(direction * INITIAL_FORCE).with_persistence(false), // Doesn't seem to work sometimes?
-                    Restitution::new(1.),
+                    DelayedExternalForce(
+                        ExternalForce::new(direction * INITIAL_FORCE).with_persistence(false),
+                    ),
+                    Restitution::new(0.7),
                     Friction::ZERO,
                     LockedAxes::ROTATION_LOCKED,
-                    Ball {
-                        kind: ball_kind
-                    }
+                    Ball { kind: ball_kind },
                 ));
             }
         }
     }
 }
 
-fn get_ball_color (
-    ball_kind: &BallKind
-) -> Color {
+fn apply_delayed_external_forces(
+    mut commands: Commands,
+    q: Query<(Entity, &DelayedExternalForce)>,
+) {
+    for (e, d) in q.iter() {
+        commands
+            .entity(e)
+            .insert(d.0)
+            .remove::<DelayedExternalForce>();
+    }
+}
+
+fn get_ball_color(ball_kind: &BallKind) -> Color {
     // Not color-blind friendly, replace with specific sprites instead
     match ball_kind {
         BallKind::Point => Color::WHITE,
@@ -376,7 +367,7 @@ fn get_ball_color (
     }
 }
 
-fn respawn_point_ball (
+fn respawn_point_ball(
     mut point_ball_count: ResMut<PointBallCount>,
     mut ball_spawner: ResMut<BallSpawner>,
 ) {
@@ -402,12 +393,11 @@ fn spawn_bonus_ball(
     stopwatch.tick(time.delta());
     if stopwatch.elapsed_secs() > 8. {
         let rand = rand::random::<f32>();
-        let kind =
-            if rand < 0.2 {
-                BallKind::Multi
-            } else {
-                BallKind::SwitchSide
-            };
+        let kind = if rand < 0.2 {
+            BallKind::Multi
+        } else {
+            BallKind::SwitchSide
+        };
         ball_spawner.0.push_back((kind, Side::Random));
         stopwatch.reset();
     }
@@ -459,8 +449,12 @@ fn check_goals(
                     point_ball_count.0 -= 1;
                 }
                 BallKind::Multi => {
-                    ball_spawner.0.push_front((get_point_ball(), goal.side.clone()));
-                    ball_spawner.0.push_front((get_point_ball(), goal.side.clone()));
+                    ball_spawner
+                        .0
+                        .push_front((get_point_ball(), goal.side.clone()));
+                    ball_spawner
+                        .0
+                        .push_front((get_point_ball(), goal.side.clone()));
                 }
                 BallKind::SwitchSide => {
                     let mut paddles = paddles.iter_mut();
@@ -485,34 +479,49 @@ fn check_goals(
 }
 
 fn move_paddle(
-    keys: Res<Input<KeyCode>>,
-    mut paddles: Query<(&mut LinearVelocity, &Paddle)>,
+    time: Res<Time>,
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+    buttons: Res<Input<MouseButton>>,
+    camera: Query<(&GlobalTransform, &Camera)>,
+    mut paddles: Query<(&Transform, &mut LinearVelocity, &Paddle)>,
+    balls: Query<(&Transform), With<Ball>>,
 ) {
     // Probably not the correct way to move a KinematicBody, seems to ignore walls and net.
     // I tried modifying the Transform and Position also to no avail.
-    for (mut velocity, paddle) in paddles.iter_mut() {
+
+    for (paddle_transform, mut velocity, paddle) in paddles.iter_mut() {
         if paddle.first_player {
-            let mut new_velocity = Vec2::ZERO;
-            if keys.pressed(KeyCode::Up) {
-                new_velocity.y += PADDLE_SPEED;
+            if !buttons.pressed(MouseButton::Left) {
+                *velocity = LinearVelocity(Vec2::ZERO);
+                continue;
             }
-            if keys.pressed(KeyCode::Down) {
-                new_velocity.y -= PADDLE_SPEED;
-            }
-            if keys.pressed(KeyCode::Left) {
-                new_velocity.x -= PADDLE_SPEED;
-            }
-            if keys.pressed(KeyCode::Right) {
-                new_velocity.x += PADDLE_SPEED;
-            }
+            let Some(position) = q_windows.single().cursor_position() else {
+                    continue;
+                };
+            let Some((camera_transform, camera)) = camera.iter().next() else {
+                continue;
+                };
+            let Some(position) = camera.viewport_to_world_2d(camera_transform, position) else {
+                continue;
+                };
+            let to_target_position = position - paddle_transform.translation.xy();
+            let new_velocity = to_target_position.normalize_or_zero()
+                * PADDLE_SPEED.min(to_target_position.length() / time.delta_seconds());
+            *velocity = LinearVelocity(new_velocity);
+        } else {
+            let Some(t) = balls.iter().next() else {
+                continue;
+            };
+            let to_target_position = Vec2::new(paddle_transform.translation.x, t.translation.y)
+                - paddle_transform.translation.xy();
+            let new_velocity = to_target_position.normalize_or_zero()
+                * PADDLE_SPEED_AI.min(to_target_position.length() / time.delta_seconds());
             *velocity = LinearVelocity(new_velocity);
         }
     }
 }
 
-fn print_score (
-    score: Res<Score>
-) {
+fn print_score(score: Res<Score>) {
     if score.is_changed() {
         println!("Score: {}:{}", score.first_player, score.second_player);
     }
