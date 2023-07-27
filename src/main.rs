329809bb40b0e1fb17a68b5c772a617b9a1258e7
mramirez <ramirezmike2@gmail.com>
2023-07-27 17:36:50 -0400

    setup score, added bounds, added score indicators on the balls

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,22 @@ use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 use bevy::time::Stopwatch;
 use bevy::window::{PrimaryWindow, WindowResolution};
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 
 use bevy_xpbd_2d::prelude::*;
 
 const WINDOW_SIZE: Vec2 = Vec2 { x: 1280., y: 720. };
 
 const PADDLE_SIZE: Vec2 = Vec2 { x: 15., y: 60. };
+
+const P1_LEFT_BOUND: f32 = -(-PADDLE_SIZE.x + WINDOW_SIZE.x * 0.5);
+const P1_RIGHT_BOUND: f32 = -(PADDLE_SIZE.x + WINDOW_SIZE.x * 0.25);
+const P1_TOP_BOUND: f32 = PADDLE_SIZE.y + WINDOW_SIZE.y * 0.5;
+const P1_BOTTOM_BOUND: f32 = -(PADDLE_SIZE.y + WINDOW_SIZE.y * 0.5);
+const LEFT_WALL: f32 = -(WINDOW_SIZE.x * 0.5);
+const RIGHT_WALL: f32 = -(WINDOW_SIZE.x * 0.25);
+const TOP_WALL: f32 = WINDOW_SIZE.y * 0.5;
+const BOTTOM_WALL: f32 = -(WINDOW_SIZE.y * 0.5);
 
 const BALL_RADIUS: f32 = 15.;
 
@@ -21,8 +31,8 @@ const PADDLE_SPEED_AI: f32 = 500.;
 
 #[derive(Resource, Default)]
 struct Score {
-    first_player: u8,
-    second_player: u8,
+    first_player: usize,
+    second_player: usize,
 }
 
 #[derive(Component, Debug)]
@@ -39,36 +49,29 @@ struct Goal {
 
 #[derive(Component)]
 struct Ball {
-    kind: BallKind,
+    points: usize,
 }
 
-#[derive(Component)]
-struct DelayedExternalForce(pub ExternalForce);
-
-#[derive(Clone, Debug)]
-enum BallKind {
-    Point,
-    Gold,
-    Multi,
-    SwitchSide,
-}
-
-impl BallKind {
-    fn is_bonus(&self) -> bool {
-        match self {
-            Self::Point | Self::Gold => false,
-            _ => true,
-        }
-    }
-
+impl Ball {
     fn get_radius(&self) -> f32 {
-        if self.is_bonus() {
+        if self.points > 0 {
             BALL_RADIUS * 0.85
         } else {
             BALL_RADIUS
         }
     }
 }
+
+#[derive(Component)]
+struct BallTextMarker;
+#[derive(Component)]
+struct Player1ScoreMarker;
+#[derive(Component)]
+struct Player2ScoreMarker;
+
+#[derive(Component)]
+struct DelayedExternalForce(pub ExternalForce);
+
 
 #[derive(Resource, Default)]
 struct PointBallCount(u8);
@@ -77,9 +80,6 @@ struct PointBallCount(u8);
 struct BallAssets {
     point_ball: Handle<Image>,
 }
-
-#[derive(Resource, Default)]
-struct BallSpawner(VecDeque<(BallKind, Side)>);
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 enum Side {
@@ -107,7 +107,12 @@ enum Layer {
 }
 
 fn main() {
-    App::new()
+    let mut app = App::new();
+
+    #[cfg(feature = "fps")]
+    app.add_plugins((LogDiagnosticsPlugin::default(), FrameTimeDiagnosticsPlugin::default()));
+
+    app
         .add_plugins((
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
@@ -126,13 +131,11 @@ fn main() {
                 apply_delayed_external_forces,
                 (
                     check_goals,
-                    respawn_point_ball,
-                    spawn_bonus_ball,
                     spawn_ball,
                 )
                     .chain(),
                 move_paddle,
-                print_score,
+                update_score.run_if(resource_changed::<Score>()),
             ),
         )
         .run();
@@ -145,7 +148,6 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     });
 
     // Spawner
-    commands.init_resource::<BallSpawner>();
     commands.init_resource::<PointBallCount>();
 
     // Score
@@ -229,6 +231,57 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     // Paddles
     spawn_paddle(&mut commands, true);
     spawn_paddle(&mut commands, false);
+
+    // Game UI including Score Display
+    commands.spawn(NodeBundle {
+        style: Style { 
+            width: Val::Percent(100.),
+            height: Val::Percent(20.),
+            display: Display::Flex,
+            flex_direction: FlexDirection::Row,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        ..default()
+    })
+    .with_children(|builder| {
+        builder.spawn(NodeBundle {
+            style: Style { 
+                width: Val::Percent(20.),
+                height: Val::Percent(20.),
+                display: Display::Flex,
+                flex_direction: FlexDirection::Row,
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|builder| {
+            builder.spawn((TextBundle {
+                text: Text::from_section(
+                    "0",
+                    TextStyle {
+                        font_size: 50.0,
+                        color: Color::WHITE,
+                        ..default()
+                    },
+                ).with_alignment(TextAlignment::Left),
+                ..default()
+            }, Player1ScoreMarker));
+
+            builder.spawn((TextBundle {
+                text: Text::from_section(
+                    "0",
+                    TextStyle {
+                        font_size: 50.0,
+                        color: Color::WHITE,
+                        ..default()
+                    },
+                ).with_alignment(TextAlignment::Right),
+                ..default()
+            }, Player2ScoreMarker));
+        });
+    });
 }
 
 fn mirror_transform(transform: &mut Transform) {
@@ -292,60 +345,76 @@ fn spawn_wall(
 fn spawn_ball(
     mut commands: Commands,
     ball_assets: Res<BallAssets>,
-    mut ball_spawner: ResMut<BallSpawner>,
     spatial_query: SpatialQuery,
     mut timer: Local<Timer>,
     time: Res<Time>,
 ) {
     timer.tick(time.delta());
     if timer.finished() {
-        if let Some((ball_kind, _)) = ball_spawner.0.front() {
-            let ball_collider = Collider::ball(ball_kind.get_radius());
-            let ball_position = Vec2::ZERO;
-            let intersections = spatial_query.shape_intersections(
-                &ball_collider,
-                ball_position,
-                0.,
-                SpatialQueryFilter::new().with_masks([Layer::Ball, Layer::Paddle]),
-            );
+        let ball_collider = Collider::ball(BALL_RADIUS);
+        let ball_position = Vec2::ZERO;
+        let intersections = spatial_query.shape_intersections(
+            &ball_collider,
+            ball_position,
+            0.,
+            SpatialQueryFilter::new().with_masks([Layer::Ball, Layer::Paddle]),
+        );
 
-            if intersections.is_empty() {
-                let (ball_kind, spawn_direction) = ball_spawner.0.pop_front().unwrap();
+        if intersections.is_empty() {
+            let spawn_direction = Side::Random;
 
-                timer.set_duration(Duration::from_secs(1));
-                timer.reset();
+            timer.set_duration(Duration::from_millis(10));
+            timer.reset();
 
-                let direction_angle = rand::random::<f32>() * (PI / 2. - PI / 4.);
-                let mut direction = Vec2::from_angle(direction_angle);
-                if spawn_direction == Side::Left
-                    || (spawn_direction == Side::Random && rand::random::<bool>())
-                {
-                    direction.x = -direction.x;
-                }
+            let direction_angle = rand::random::<f32>() * (PI / 2. - PI / 4.);
+            let mut direction = Vec2::from_angle(direction_angle);
+            if spawn_direction == Side::Left
+                || (spawn_direction == Side::Random && rand::random::<bool>())
+            {
+                direction.x = -direction.x;
+            }
 
-                commands.spawn((
-                    SpriteBundle {
-                        texture: ball_assets.point_ball.clone(),
-                        sprite: Sprite {
-                            color: get_ball_color(&ball_kind),
-                            custom_size: Some(Vec2::ONE * (BALL_RADIUS * 2.)),
-                            ..default()
-                        },
-                        transform: Transform::from_translation(ball_position.extend(4.)),
+            commands.spawn((
+                SpriteBundle {
+                    texture: ball_assets.point_ball.clone(),
+                    sprite: Sprite {
+                        color: Color::WHITE,
+                        custom_size: Some(Vec2::ONE * (BALL_RADIUS * 2.)),
                         ..default()
                     },
-                    RigidBody::Dynamic,
-                    ball_collider,
-                    CollisionLayers::new([Layer::Ball], [Layer::Ball, Layer::Paddle, Layer::Wall]),
-                    DelayedExternalForce(
-                        ExternalForce::new(direction * INITIAL_FORCE).with_persistence(false),
-                    ),
-                    Restitution::new(0.7),
-                    Friction::ZERO,
-                    LockedAxes::ROTATION_LOCKED,
-                    Ball { kind: ball_kind },
-                ));
-            }
+                    transform: Transform::from_translation(ball_position.extend(4.)),
+                    ..default()
+                },
+                RigidBody::Dynamic,
+                ball_collider,
+                CollisionLayers::new([Layer::Ball], [Layer::Ball, Layer::Paddle, Layer::Wall]),
+                DelayedExternalForce(
+                    ExternalForce::new(direction * INITIAL_FORCE).with_persistence(false),
+                ),
+                Restitution::new(0.7),
+                Friction::ZERO,
+                LockedAxes::ROTATION_LOCKED,
+                Ball { points: 0 },
+            ))
+            .with_children(|builder| {
+                builder.spawn((
+                    Text2dBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "0",
+                                TextStyle {
+                                    font_size: 20.0,
+                                    color: Color::WHITE,
+                                    ..default()
+                                },
+                            )],
+                            ..Default::default()
+                        },
+                        transform: Transform::from_xyz(0.0, 0.0, 99.0),
+                        ..default()
+                    }
+                ,BallTextMarker));
+            });
         }
     }
 }
@@ -362,52 +431,6 @@ fn apply_delayed_external_forces(
     }
 }
 
-fn get_ball_color(ball_kind: &BallKind) -> Color {
-    // Not color-blind friendly, replace with specific sprites instead
-    match ball_kind {
-        BallKind::Point => Color::WHITE,
-        BallKind::Gold => Color::YELLOW,
-        BallKind::SwitchSide => Color::GREEN,
-        BallKind::Multi => Color::BLUE,
-    }
-}
-
-fn respawn_point_ball(
-    mut point_ball_count: ResMut<PointBallCount>,
-    mut ball_spawner: ResMut<BallSpawner>,
-) {
-    if point_ball_count.0 == 0 {
-        ball_spawner.0.push_front((get_point_ball(), Side::Random));
-        point_ball_count.0 = 1;
-    }
-}
-
-fn get_point_ball() -> BallKind {
-    if rand::random::<f32>() < 0.02 {
-        BallKind::Gold
-    } else {
-        BallKind::Point
-    }
-}
-
-fn spawn_bonus_ball(
-    mut ball_spawner: ResMut<BallSpawner>,
-    mut stopwatch: Local<Stopwatch>,
-    time: Res<Time>,
-) {
-    stopwatch.tick(time.delta());
-    if stopwatch.elapsed_secs() > 8. {
-        let rand = rand::random::<f32>();
-        let kind = if rand < 0.2 {
-            BallKind::Multi
-        } else {
-            BallKind::SwitchSide
-        };
-        ball_spawner.0.push_back((kind, Side::Random));
-        stopwatch.reset();
-    }
-}
-
 fn check_goals(
     mut commands: Commands,
     mut collision_event_reader: EventReader<Collision>,
@@ -416,7 +439,6 @@ fn check_goals(
     mut score: ResMut<Score>,
     mut point_ball_count: ResMut<PointBallCount>,
     mut paddles: Query<(&mut Position, &mut Paddle)>,
-    mut ball_spawner: ResMut<BallSpawner>,
 ) {
     for Collision(contact) in collision_event_reader.iter() {
         if let Some((goal, _goal_entity, ball, ball_entity)) =
@@ -436,54 +458,38 @@ fn check_goals(
                 None
             }
         {
-            match ball.kind {
-                BallKind::Point => {
-                    if goal.first_player {
-                        score.first_player += 1;
-                    } else {
-                        score.second_player += 1;
-                    }
-                    if let Some(new_score) = point_ball_count.0.checked_sub(1) {
-                        point_ball_count.0 = new_score;
-                    }
-                }
-                BallKind::Gold => {
-                    if goal.first_player {
-                        score.first_player += 3;
-                    } else {
-                        score.second_player += 3;
-                    }
-                    if let Some(new_score) = point_ball_count.0.checked_sub(1) {
-                        point_ball_count.0 = new_score;
-                    }
-                }
-                BallKind::Multi => {
-                    ball_spawner
-                        .0
-                        .push_front((get_point_ball(), goal.side.clone()));
-                    ball_spawner
-                        .0
-                        .push_front((get_point_ball(), goal.side.clone()));
-                }
-                BallKind::SwitchSide => {
-                    let mut paddles = paddles.iter_mut();
-                    let (mut first_transform, mut first_paddle) = paddles.next().unwrap();
-                    let (mut second_transform, mut second_paddle) = paddles.next().unwrap();
-
-                    first_paddle.side = first_paddle.side.opposite();
-                    second_paddle.side = second_paddle.side.opposite();
-
-                    let temp = first_transform.clone();
-                    *first_transform = second_transform.clone();
-                    *second_transform = temp;
-
-                    for mut goal in goals.iter_mut() {
-                        goal.first_player = !goal.first_player;
-                    }
-                }
+            if goal.first_player {
+                score.first_player += 1;
+            } else {
+                score.second_player += 1;
             }
-            commands.get_entity(ball_entity).unwrap().despawn();
+            if let Some(new_score) = point_ball_count.0.checked_sub(1) {
+                point_ball_count.0 = new_score;
+            }
+
+            if let Some(entity) = commands.get_entity(ball_entity) {
+                entity.despawn_recursive();
+            }
         }
+
+//      if let Some((goal, _goal_entity, ball, ball_entity)) =
+//          if let Ok((_, paddle)) = paddles.get(contact.entity1) {
+//              if let Ok(ball) = balls.get(contact.entity2) {
+//                  Some((goal, contact.entity1, ball, contact.entity2))
+//              } else {
+//                  None
+//              }
+//          } else if let Ok(goal) = goals.get(contact.entity2) {
+//              if let Ok(ball) = balls.get(contact.entity1) {
+//                  Some((goal, contact.entity2, ball, contact.entity1))
+//              } else {
+//                  None
+//              }
+//          } else {
+//              None
+//          }
+//      {
+//      }
     }
 }
 
@@ -492,13 +498,10 @@ fn move_paddle(
     q_windows: Query<&Window, With<PrimaryWindow>>,
     buttons: Res<Input<MouseButton>>,
     camera: Query<(&GlobalTransform, &Camera)>,
-    mut paddles: Query<(&Transform, &mut LinearVelocity, &Paddle)>,
-    balls: Query<(&Transform), With<Ball>>,
+    mut paddles: Query<(&Transform, &mut LinearVelocity, &mut Position, &Paddle)>,
+    balls: Query<&Transform, With<Ball>>,
 ) {
-    // Probably not the correct way to move a KinematicBody, seems to ignore walls and net.
-    // I tried modifying the Transform and Position also to no avail.
-
-    for (paddle_transform, mut velocity, paddle) in paddles.iter_mut() {
+    for (paddle_transform, mut velocity, mut paddle_position, paddle) in paddles.iter_mut() {
         if paddle.first_player {
             if !buttons.pressed(MouseButton::Left) {
                 *velocity = LinearVelocity(Vec2::ZERO);
@@ -516,13 +519,32 @@ fn move_paddle(
             let to_target_position = position - paddle_transform.translation.xy();
             let new_velocity = to_target_position.normalize_or_zero()
                 * PADDLE_SPEED.min(to_target_position.length() / time.delta_seconds());
-            *velocity = LinearVelocity(new_velocity);
+            velocity.0 = new_velocity;
+
+            // keeping paddle within bounds 
+            if paddle_position.x < P1_LEFT_BOUND && velocity.x < 0.0 {
+                velocity.x = 0.0;
+                paddle_position.x = LEFT_WALL;
+            }
+            if paddle_position.x > P1_RIGHT_BOUND && velocity.x > 0.0 {
+                paddle_position.x = RIGHT_WALL;
+                velocity.x = 0.0;
+            }
+            if paddle_position.y > P1_TOP_BOUND && velocity.y > 0.0 {
+                velocity.y = 0.0;
+                paddle_position.y = TOP_WALL;
+            }
+            if paddle_position.y < P1_BOTTOM_BOUND && velocity.y < 0.0 {
+                velocity.y = 0.0;
+                paddle_position.y = BOTTOM_WALL;
+            }
         } else {
             let Some(t) = balls.iter().next() else {
                 continue;
             };
             let to_target_position = Vec2::new(paddle_transform.translation.x, t.translation.y)
                 - paddle_transform.translation.xy();
+
             let new_velocity = to_target_position.normalize_or_zero()
                 * PADDLE_SPEED_AI.min(to_target_position.length() / time.delta_seconds());
             *velocity = LinearVelocity(new_velocity);
@@ -530,8 +552,14 @@ fn move_paddle(
     }
 }
 
-fn print_score(score: Res<Score>) {
-    if score.is_changed() {
-        println!("Score: {}:{}", score.first_player, score.second_player);
-    }
+fn update_score(
+    score: Res<Score>, 
+    mut player_1_score: Query<&mut Text, (With<Player1ScoreMarker>, Without<Player2ScoreMarker>)>,
+    mut player_2_score: Query<&mut Text, (With<Player2ScoreMarker>, Without<Player1ScoreMarker>)>,
+) {
+    let mut p1_text = player_1_score.single_mut();
+    p1_text.sections[0].value = format!("{}", score.first_player);
+
+    let mut p2_text = player_2_score.single_mut();
+    p2_text.sections[0].value = format!("{}", score.second_player);
 }
